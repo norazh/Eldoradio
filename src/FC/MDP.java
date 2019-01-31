692d0bb387e225f3c42340a99fa3c4e47a327e3a
menardtiphaine <menard.tiphaine22100@gmail.com>
2019-01-31 12:02:43 +0100

    MDP caractères spéciaux 31/01

--- src/FC/MDP.java
+++ src/FC/MDP.java
@@ -12,15 +12,15 @@ package FC;
 public class MDP {
     private String mdp;
     public MDP (String mdp){
+        if (MdpValide(mdp)){
         this.mdp=mdp;
-        
-    /**
-     *
-     * @param mdp
-     * @return
-     */
+        }
+        else{
+            System.out.println("Mot de passe incorrect. Le mdp doit avoir au minimum 5 caractères, au minimum 1 majuscule, 1 minuscule, au moins 1 chiffre et au moins u caractère spécial");
+        }
+    }
     public boolean MdpValide(String mdp){
-        // 5 caractères minimum
+        // 8 caractères minimum
         // 1 majuscule minimum
         // 1 minuscule minimum
         // 1 chiffre minimum
@@ -31,20 +31,24 @@ public class MDP {
         boolean minuscule = false;
         boolean lettre = false;
         boolean nombre=false;
+        boolean charSpecial=false;
         
-        if(mdp.length()<5){
-            System.out.println("Mot de Passe invalide : trop petit");
+        if(mdp.length()<8){
+            System.out.println("Mot de Passe invalide : trop petit. Il doit faire au moins 8 caractères");
             return false;
         }
         
-            while(i<mdp.length() && (majuscule==false || minuscule==false || lettre==false || nombre==false)){
+            while(i<mdp.length() && (majuscule==false || minuscule==false || lettre==false || nombre==false || charSpecial==false)){
             c = mdp.charAt(i);
             if((c>='0')&&(c<='9')){
   
                 nombre = true;
               }
+            else if ((c=='%') ||(c=='#') || (c==':') || (c=='$') || (c=='*')|| (c=='!')|| (c=='"')|| (c=='&')|| (c=='+')|| (c==',')|| (c=='-')|| (c=='.')|| (c=='/')|| (c==';')|| (c=='<')|| (c=='=')|| (c=='>')|| (c=='?')|| (c=='@')|| (c=='^')|| (c=='_')|| (c=='~')) {
+                
+                    charSpecial=true;
+            }
             else{
-
                 lettre = true;
             }
             if((c>='a')&&(c<='z')){
@@ -58,7 +62,7 @@ public class MDP {
             i++;
             
         }
-        if(lettre && majuscule && minuscule && nombre){
+        if(lettre && majuscule && minuscule && nombre && charSpecial){
             return true;
         }
         else{
