6e7d0159b84a59f404fb1ef8b5860b6bea26e506
Loïc <ldancelme@gmail.com>
2019-03-07 01:01:25 +0100

    MAJ Classe DbConnexion
    methodes select() et insert() pour intéragir avec la BD

---
+++ src/UI/Main.java
@@ -0,0 +1,14 @@
+package UI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new AccueilInterne();
+            }
+        });
+
+    }
+
+}
